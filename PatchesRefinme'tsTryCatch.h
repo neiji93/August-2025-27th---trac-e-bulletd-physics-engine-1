60b10b6b5c76ad38a57c09828e4ce3fcdf435ec4
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-08-27 13:49:28 +0200

    Create PatchesRefinme'tsTryCatch.h

---
+++ PatchesRefinme'tsTryCatch.h
@@ -0,0 +1,4 @@
+//we can use ai too. then modelling will 
+//produce movements equations because of several isomorphisms.
+//as this is not what we want, we have to use a stack
+//to cancel some parameters. after that, this is really a modelling problem.
